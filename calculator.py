94ee5b3da51824e384d08700d448f1cbbeab7971
Antonio Ruiz <3speedemon3@gmail.com>
2023-11-10 19:42:32 +0100

    Subiendo archivo con las funciones a probar

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# Lab 5 -> Antonio Ruiz Benito
+
+def add (a, b):
+    """Metodo para sumar 2 numeros pasados por parametro"""
+    return a + b
+
+def subtract (a, b ):
+    """Metodo para restar 2 numeros pasados por parametro"""
+    return a - b
+
+def multiply (a, b):
+    """Metodo para multiplicar 2 numeros pasados por parametro"""
+    return a * b
+
+def divide (a, b):
+    """
+    Metodo para dividir 2 numeros pasados por parametro,
+    controlando la excepcion que pudiese ocurrir al dividir por 0
+    """
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return print("You can´t divide by zero!!!")
+
+
+if __name__ == "__main__":
+    print(sum(3, 9))
+    print(subtract(27, 2))
+    print(multiply(1987, 29))
+    print(divide(100, 9))
